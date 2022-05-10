c0143c79e0aab173c35c203373b0241d03e9125e
asavoullis <cs16538@bristol.ac.uk>
2022-05-10 03:07:43 +0000

    updates with more leetcode

--- examples_and_tasks4.py
+++ examples_and_tasks4.py
@@ -141,7 +141,7 @@ for key, value in person_info.items():
     setattr(person2, key, value)
 
 
-"""Task10: Given an array of ints, return True if the array contains a 2 next to a 2 somewhere. """
+"""Task10: List-2 > has22. Given an array of ints, return True if the array contains a 2 next to a 2 somewhere. """
 def has22(nums):
     for x in range(len(nums) - 1):
         if (nums[x] == 2) and (nums[x + 1] == 2):
@@ -153,8 +153,8 @@ def has22_v2(nums):
     return any(x == y == 2 for x, y in zip(nums, nums[1:]))
 
 
-"""Task11: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending
-to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. """
+"""Task11: List-2 > sum67. Return the sum of the numbers in the array, except ignore sections of numbers starting with 
+a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. """
 def sum67(nums):
     result = 0
     startadding = True
@@ -168,8 +168,8 @@ def sum67(nums):
     return result
 
 
-"""Task12: Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very 
-unlucky, so it does not count and numbers that come immediately after a 13 also do not count. """
+"""Task12: List-2 > sum13. Return the sum of the numbers in the array, returning 0 for an empty array. Except the number
+ 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count. """
 def sum13(nums):
     result = 0
     pause = False
@@ -181,6 +181,39 @@ def sum13(nums):
         else:
             pause = False
     return result
+
+
+"""Task13: List-2 > centered_average. Return the "centered" average of an array of ints, which we'll say is the mean 
+average of the values, except ignoring the largest and smallest values in the array. If there are multiple 
+copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce 
+the final average. You may assume that the array is length 3 or more. """
+def centered_average(nums):
+    sum = 0
+    for element in nums:
+        sum += element
+    return (sum - min(nums) - max(nums)) / (len(nums)-2)
+
+
+"""Task14: List-2 > big_diff. Given an array length 1 or more of ints, return the difference between the largest and 
+smallest values in the array. Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or 
+larger of two values. """
+def big_diff(nums):
+    return max(nums) - min(nums)
+
+
+"""Task15: List-2 > count_evens. Return the number of even ints in the given array. Note: the % "mod" operator computes 
+the remainder, e.g. 5 % 2 is 1."""
+def count_evens(nums):
+    count = 0
+    for num in nums:
+        if num % 2 == 0:
+            count += 1
+    return count
+
+
+
+
+
 
 
 def main():
@@ -228,6 +261,22 @@ def main():
     print(sum13([13, 1, 2, 13, 2, 1, 13])) #2+1=3
     print(sum13([1, 2, 13, 2, 1, 13])) #1+2+1=4
     print(sum13([5, 13, 2]), '\n') #5
+
+    print('Task13: List-2 > centered_average. ')
+    print(centered_average([-10, -4, -2, -4, -2, 0])) #3
+    print(centered_average([0, 2, 3, 4, 100])) #3
+    print(centered_average([-10, -4, -2, -4, -2, 0]), '\n') #-3
+
+    print("Task14: List-2 > big_diff. ")
+    print(big_diff([5, 1, 6, 1, 9, 9])) #8
+    print(big_diff([7, 7, 6, 8, 5, 5, 6])) #3
+    print(big_diff([10, 0])) #10
+    print(big_diff([2, 2]), '\n') #0
+
+    print("Task15: List-2 > count_evens. ")
+    print(count_evens([2, 11, 9, 0])) #2
+    print(count_evens([1, 3, 5])) #0
+    print(count_evens([2, 5, 12]), '\n') #2
 
 
 
