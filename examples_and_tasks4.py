0de8145c89f2a0328bfcd65e2f6aa9ca73828f8e
asavoullis <cs16538@bristol.ac.uk>
2022-05-10 01:11:42 +0000

    updates with more leetcode

--- examples_and_tasks4.py
+++ examples_and_tasks4.py
@@ -100,8 +100,10 @@ def tricky_assignemnt2():
 class Person():
     pass
 
+
 def useful_function(x):
     return x * x
+
 
 class UsefulClass:
     def __init__(self, x):
@@ -118,10 +120,10 @@ first_key = 'first'
 first_val = 'Corey'
 
 # setattr is able to use the value of a variable
-# pass in the object that we want to set an attirubute on
+# pass in the object that we want to set an attribute on
 setattr(person, first_key, first_val)
 
-# we want to get the first attirubute here
+# we want to get the first attribute here
 first = getattr(person, first_key)
 
 
@@ -141,9 +143,6 @@ for key, value in person_info.items():
 
 """Task10: Given an array of ints, return True if the array contains a 2 next to a 2 somewhere. """
 def has22(nums):
-# has22([1, 2, 2]) → True
-#has22([1, 2, 1, 2]) → False
-# has22([2, 1, 2]) → False
     for x in range(len(nums) - 1):
         if (nums[x] == 2) and (nums[x + 1] == 2):
             return True
@@ -154,10 +153,34 @@ def has22_v2(nums):
     return any(x == y == 2 for x, y in zip(nums, nums[1:]))
 
 
-"""Task11:Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending\
- to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. """
+"""Task11: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending
+to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. """
 def sum67(nums):
-    pass
+    result = 0
+    startadding = True
+    for val in nums:
+        if val == 6:
+            startadding = False
+        if startadding:
+            result += val
+        if val == 7:
+            startadding = True
+    return result
+
+
+"""Task12: Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very 
+unlucky, so it does not count and numbers that come immediately after a 13 also do not count. """
+def sum13(nums):
+    result = 0
+    pause = False
+    for num in nums:
+        if pause == False and num != 13:
+            result += num
+        if num == 13:
+            pause = True
+        else:
+            pause = False
+    return result
 
 
 def main():
@@ -186,12 +209,26 @@ def main():
 
     print('\n')
     print('Task10: Given an array of ints, return True if the array contains a 2 next to a 2 somewhere. ')
-    print(has22([2, 1, 2]))
-    print(has22([1, 2, 1, 2]))
-    print(has22([]))
-    print(has22_v2([4, 2, 4, 2, 2, 5]), '\n')
+    print(has22([2, 1, 2])) #False
+    print(has22([1, 2, 1, 2])) #False
+    print(has22([])) #False
+    print(has22_v2([4, 2, 4, 2, 2, 5]), '\n') #True
 
-    print('')
+    print('Task11: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and '
+          'extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.')
+    print(sum67([1, 2, 2])) #5
+    print(sum67([1, 2, 2, 6, 99, 99, 7])) #5
+    print(sum67([1, 6, 7, 7])) #1+7=8
+    print(sum67([6, 7, 1, 6, 7, 7])) #1+7=8
+    print(sum67([6, 8, 1, 6, 7])) #0
+    print(sum67([]), '\n')
+
+    print('Task12: Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is '
+          'very unlucky, so it does not count and numbers that come immediately after a 13 also do not count. ')
+    print(sum13([13, 1, 2, 13, 2, 1, 13])) #2+1=3
+    print(sum13([1, 2, 13, 2, 1, 13])) #1+2+1=4
+    print(sum13([5, 13, 2]), '\n') #5
+
 
 
 # dunder == double underscore
